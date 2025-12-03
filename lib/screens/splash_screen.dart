bcd2bf91dd9dc2f957df0e906e871d78a9ad15d6
anjal <anjalmani2023@gmail.com>
2025-12-03 22:58:03 +0545

    Splash Screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/onboarding');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset("assets/images/logo.png", width: 200, height: 200),
+      ),
+    );
+  }
+}
