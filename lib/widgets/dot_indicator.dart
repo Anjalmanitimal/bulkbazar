6637da787cb01c46847326e2449fd1b26e671715
anjal <anjalmani2023@gmail.com>
2025-12-04 23:08:20 +0545

    widgtes dot_indicator

---
+++ lib/widgets/dot_indicator.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DotIndicator extends StatelessWidget {
+  final bool isActive;
+
+  const DotIndicator({super.key, required this.isActive});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      height: 8,
+      width: isActive ? 20 : 8,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.black : Colors.grey.shade400,
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+}
