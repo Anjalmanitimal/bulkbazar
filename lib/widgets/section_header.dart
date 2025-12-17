6217858a7e804244e4d3bb836dbe0d24e42e39d2
anjal <anjalmani2023@gmail.com>
2025-12-18 00:32:57 +0545

    section header widget

---
+++ lib/widgets/section_header.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const SectionHeader({super.key, required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                subtitle,
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ],
+          ),
+          const Text("View all", style: TextStyle(color: Colors.grey)),
+        ],
+      ),
+    );
+  }
+}
